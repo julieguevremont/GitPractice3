256b556e17196e4af7a71182a33df1808290694a
Julie Guevremont <julieguevremont@Julies-MacBook-Pro.local>
2017-08-20 22:11:04 -0400

    Added new project

---
+++ GitPractice3/GitPractice3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitPractice3
+//
+//  Created by Julie Guevremont on 2017-08-20.
+//  Copyright © 2017 Julie Guevremont. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
